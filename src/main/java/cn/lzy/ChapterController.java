2480dcb44817bc3b864ed3ae7a71a6fdb5d01068
yeyucs <2556368956@qq.com>
2023-06-10 16:37:27 +0800

    修改了controller

--- src/main/java/cn/lzy/ChapterController.java
+++ src/main/java/cn/lzy/ChapterController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChapterController {
     @RequestMapping(value = "/hello")
     public String sayHello(){
+        System.out.println("Hello 大家好");
         return "hello Spring boot !!";
     }
 }
