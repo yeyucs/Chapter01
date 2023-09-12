ba6d93524b1a6c28704d397d86291a84c0075eed
yeyucs <2556368956@qq.com>
2023-09-12 12:04:52 +0800

    web，Mvc

---
+++ src/main/java/cn/lzy/web/functionextensionmvc/MyInterceptor.java
@@ -0,0 +1,54 @@
+package cn.lzy.web.functionextensionmvc;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import reactor.util.annotation.Nullable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+
+@Component
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,Object handler) throws Exception{
+        String uri = request.getRequestURI();
+        Object loginUser = request.getSession().getAttribute("loginUser");
+        if (uri.startsWith("/admin") && null == loginUser) {
+            response.sendRedirect("/toLoginPage");
+            return false;
+        }
+        return true;
+    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        request.setAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
