993cd457f15fe11bbdecb8a472a8e0b8f2b06af0
yeyucs <2556368956@qq.com>
2023-10-24 09:49:39 +0800

    定时任务实现 优化1

--- src/main/java/cn/lzy/taskmanagement/Scheduled/ScheduledService.java
+++ src/main/java/cn/lzy/taskmanagement/Scheduled/ScheduledService.java
@@ -11,33 +11,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class ScheduledService {
 
-   /* @Scheduled(fixedRate = 10*1000)
+    /*@Scheduled(fixedRate = 10*1000)
     public void scheduledServiceRate1() throws  InterruptedException {
         System.out.println("scheduledServiceRate1====="+ DataUtils.getData(DataUtils.data1));
         Thread.sleep(5*1000);
     }
-    @Scheduled(fixedRate = 15000)
+    @Scheduled(fixedRate = 1000)
     public void scheduledServiceRate2() throws InterruptedException {
         System.out.println("scheduledServiceRate2=====" + DataUtils.getData(DataUtils.data1));
         Thread.sleep(15 * 1000);
-    }
-    @Scheduled(fixedDelay = 10*1000)
+    }*/
+    /*@Scheduled(fixedDelay = 5*1000)
     public void scheduledServiceDelay1() throws InterruptedException {
         System.out.println("scheduledServiceDelay1=====" + DataUtils.getData(DataUtils.data1));
         Thread.sleep(5*1000);
-    }
-    @Scheduled(fixedDelay = 5*1000)
+    }*/
+    @Scheduled(fixedDelay = 1)
     public void scheduledServiceDelay2() throws InterruptedException {
         System.out.println("scheduledServiceDelay2=====" + DataUtils.getData(DataUtils.data1));
-        Thread.sleep(5*1000);
+        Thread.sleep(5000);
     }
 
-    @Scheduled(initialDelay =10*1000 ,fixedRate = 10*1000)
+    /*@Scheduled(initialDelay =10*1000 ,fixedRate = 10*1000)
     public void scheduledServiceinitialDelay1() throws  InterruptedException {
         System.out.println("scheduledServiceinitialDelay1====="+ DataUtils.getData(DataUtils.data1));
         Thread.sleep(5*1000);
     }
-    @Scheduled(initialDelay =0 ,fixedRate = 5*1000)
+    @Scheduled(initialDelay =0 ,fixedRate = 1000)
     public void scheduledServiceinitialDelay2() throws  InterruptedException {
         System.out.println("scheduledServiceinitialDelay2====="+ DataUtils.getData(DataUtils.data1));
         Thread.sleep(5*1000);
